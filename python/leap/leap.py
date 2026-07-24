6117a6aea8c3742fd72edbdd600643d7b4f12c3f
Ankit-K-Sharma7 <ankitk.shr7@gmail.com>
2026-07-24 05:47:52 +0530

    Complete Leap Year Checking Problem in Exercism.

---
+++ python/leap/leap.py
@@ -0,0 +1,8 @@
+def leap_year(year):
+    if year % 400 == 0 and year % 100 == 0: 
+        leap = True
+    elif year % 4 == 0 and year % 100 != 0:
+        leap = True
+    else:
+        leap = False
+    return leap
