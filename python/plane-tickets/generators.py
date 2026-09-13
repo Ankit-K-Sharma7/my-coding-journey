0c348daf206e075c8afe7aae99c209be001e1822
Ankit-K-Sharma7 <ankitk.shr7@gmail.com>
2026-09-13 23:47:28 +0530

    Complete Plane Tickets in Exercism.

---
+++ python/plane-tickets/generators.py
@@ -0,0 +1,83 @@
+"""Functions to automate Conda airlines ticketing system."""
+
+
+def generate_seat_letters(number):
+    """Generate a series of letters for airline seats.
+
+    Parameters:
+        number (int): Total number of seat letters to be generated.
+
+    Returns:
+        generator: A generator that yields seat letters.
+
+    Note:
+        Seat letters are generated from A to D.
+        After D the sequence starts again with A.
+        For example: A, B, C, D, A, B
+
+    """
+    seat = "ABCD"
+    for index in range(number):
+        yield seat[index % 4]
+
+
+def generate_seats(number):
+    """Generate a series of identifiers for airline seats.
+
+    Parameters:
+        number (int): The total number of seats to be generated.
+
+    Returns:
+        generator: A generator that yields seat numbers.
+
+    Note:
+        A seat number consists of the row number and the seat letter.
+        There is no row 13, and each row has 4 seats.
+
+        Seats should be sorted from low to high.
+        For example: 3C, 3D, 4A, 4B
+
+    """
+    for index , letter in enumerate(generate_seat_letters(number)):
+        row = index // 4 + 1
+        if row >= 13:
+            row += 1
+        yield f"{row}{letter}"
+
+
+def assign_seats(passengers):
+    """Assign seats to passengers.
+
+    Parameters:
+        passengers (list[str]): A list of strings containing names of passengers.
+
+    Returns:
+        dict: With passenger names as keys and seat numbers as values.
+        Example output: {"Adele": "1A", "Björk": "1B"}
+
+    """
+        
+    seats = generate_seats(len(passengers))
+    result = {}
+
+    for passenger in passengers:
+        result[passenger] = next(seats)
+
+    return result
+
+
+def generate_codes(seat_numbers, flight_id):
+    """Generate codes for a ticket.
+
+    Parameters:
+        seat_numbers (list[str]): A list of seat numbers.
+        flight_id (str): A string containing the flight identifier.
+
+    Returns:
+        generator: A generator that yields 12 character long ticket codes.
+
+    """
+
+    for seat in seat_numbers:
+        code = seat + flight_id
+        yield code + "0" * (12 - len(code))
